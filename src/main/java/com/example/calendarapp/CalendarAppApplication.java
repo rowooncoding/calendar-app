4cdd4adaf053e5df7fad8beffb1d912a35fa773b
Rowoon <dev.rowoon@gmail.com>
2025-08-08 12:09:32 +0900

    회의와 할 일 등록하기

--- src/main/java/com/example/calendarapp/CalendarAppApplication.java
+++ src/main/java/com/example/calendarapp/CalendarAppApplication.java
@@ -1,13 +1,45 @@
 package com.example.calendarapp;
 
+import ch.qos.logback.classic.joran.sanity.IfNestedWithinSecondPhaseElementSC;
+import com.example.calendarapp.event.AbstractEvent;
+import com.example.calendarapp.event.Event;
+import com.example.calendarapp.event.Meeting;
+import com.example.calendarapp.event.Todo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
 @SpringBootApplication
 public class CalendarAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CalendarAppApplication.class, args);
+        // SpringApplication.run(CalendarAppApplication.class, args);
+
+        List<AbstractEvent> list = new ArrayList<>();
+        HashSet<String> participants = new HashSet<>();
+        participants.add("danny.kim");
+
+        Meeting meeting1 = new Meeting(
+                1, "meeting1",
+                ZonedDateTime.now(), ZonedDateTime.now().plusHours(1),
+                participants, "meetingRoomA", "스터디"
+        );
+
+        list.add(meeting1);
+
+        Todo todo1 = new Todo(
+                2, "todo1",
+                ZonedDateTime.now(), ZonedDateTime.now().plusHours(2),
+                "할 일 적기"
+        );
+
+        list.add(todo1);
+
+        list.forEach(Event::print);
     }
 
 }
